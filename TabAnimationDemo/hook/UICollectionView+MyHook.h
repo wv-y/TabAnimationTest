3ee74ff57f5201704d5c3c7dadc4deda3b8da4bd
weilingyun <weilingyun@bonbonwork.com>
2022-08-10 10:47:57 +0800

    [collectionView测试]

---
+++ TabAnimationDemo/hook/UICollectionView+MyHook.h
@@ -0,0 +1,16 @@
+//
+//  UICollectionView+MyHook.h
+//  TabAnimationDemo
+//
+//  Created by 魏凌云 on 2022/8/10.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UICollectionView (MyHook)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TabAnimationDemo/hook/UICollectionView+MyHook.m
@@ -0,0 +1,12 @@
+//
+//  UICollectionView+MyHook.m
+//  TabAnimationDemo
+//
+//  Created by 魏凌云 on 2022/8/10.
+//
+
+#import "UICollectionView+MyHook.h"
+
+@implementation UICollectionView (MyHook)
+
+@end
